aa6191c07231160465316bc5e84763b93fafa4eb
Liza Liutova <liza@Lizas-MacBook-Air.local>
2018-07-27 00:07:21 -0400

    finished the required user stories

---
+++ TumblrFeed/TumblrFeed/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  TumblrFeed
+//
+//  Created by Liza Liutova on 7/26/18.
+//  Copyright © 2018 Liza Liutova. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
